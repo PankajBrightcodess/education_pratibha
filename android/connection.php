1571572a9dd620f55cd219d0abf1fd001527b568
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-03-10 17:27:12 +0530

    Update connection.php

--- android/connection.php
+++ android/connection.php
@@ -3,7 +3,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$conn=new mysqli("localhost","root","","db_pratibha");
 }
 else{
-	$conn=new MySQLi("localhost","softwarebss_shivanya","Shivanya@123#","softwarebss_shivanya");
+	$conn=new MySQLi("localhost","softwarebss_pratibha","Pratibha@123#","softwarebss_pratibha");
 }
 if($conn->connect_errno){
 	echo "connection failed";
