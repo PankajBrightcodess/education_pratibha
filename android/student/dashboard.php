386f1dd7f856195e49f4a4136894e506d6a95489
aadarsh <101623465+Bssaadarsh@users.noreply.github.com>
2022-04-02 12:52:02 +0530

    Update dashboard.php

--- android/student/dashboard.php
+++ android/student/dashboard.php
@@ -46,9 +46,8 @@ $msg = "";
                        
                        <div class="col-md-3 col-3"> 
 
-                     <a href="../executive/uploads/homework/<?php echo $value['assessment']; ?>" target="_blank"><i class="fa fa-eye"></i> </a>
-                   <!--   <iframe src="../executive/uploads/homework/<?php echo $value['assessment']; ?>" width="100%" height="50" name="pdf" < 
-                     	></iframe> -->
+                     <a href="../executive/uploads/homework/<?php echo @readfile($value['assessment']); ?>" target="_blank"><i class="fa fa-file-pdf-o" style="font-size:24px;color:red"></i> </a>
+                 
                           
                       </div>
                           <div class="col-3" style="font-size: 10px;"><strong></strong></div>
