07c29ade8bbb45fa4c542dc86a15ecf2894f066d
aadarsh <101623465+Bssaadarsh@users.noreply.github.com>
2022-04-15 11:15:30 +0530

    1

--- android/student/payment_constant.php
+++ android/student/payment_constant.php
@@ -1,6 +1,6 @@
 <?php
 	// define("API_KEY","rzp_test_RT0gbZ74mNgXBK");
-	define("API_KEY","rzp_test_HwCPXd4O8YsDzn");
+	define("API_KEY","rzp_live_JBfLZSlhFTK9Z4");
 	// define("API_KEY","rzp_live_ESJiSGIEkrg68p");
 	// define("API_KEY","rzp_test_KVV2yNPLssjS3jUvH171bc3x");
 	
