0f598b3f1eae4d3ee1f2359618757bce6d3ef924
aadarsh <101623465+Bssaadarsh@users.noreply.github.com>
2022-05-12 10:24:44 +0530

    1

--- adminpanel/connection.php
+++ adminpanel/connection.php
@@ -3,7 +3,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$conn=new mysqli("localhost","root","","db_pratibha");
 }
 else{
-	$conn=new MySQLi("localhost","softwarebss_prathibha","Prathibha@123#","softwarebss_prathibha");
+	$conn=new MySQLi("localhost","brightwm_prathibha","Prathibha@123#","brightwm_prathibha");
 }
 if($conn->connect_errno){
 	echo "connection failed";
