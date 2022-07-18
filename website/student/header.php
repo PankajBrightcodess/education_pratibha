a78b2c41a9da33a358997fa3132e9c1f45cc0efb
aadarsh <101623465+Bssaadarsh@users.noreply.github.com>
2022-07-18 17:20:40 +0530

    12

--- website/student/header.php
+++ website/student/header.php
@@ -78,7 +78,7 @@
   	  <a class="dropdown-item" href="winner-list.php"> <img src="../../images/app/10.png" width="50px" >&nbsp;&nbsp;&nbsp;Winner List</a>
   	  <a class="dropdown-item" href="user_profile.php"><img src="../../images/app/08.png" width="50px" >&nbsp;&nbsp;&nbsp;Profile</a>
   	  <a class="dropdown-item" href="pay.php"><img src="../../images/app/11.png" width="50px" >&nbsp;&nbsp;&nbsp;Pay Now</a>
-  	  	<a href="history.php"><img src="../../images/app/11.png" width="30px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;History</a><hr>
+  	  	<a class="dropdown-item" href="history.php"><img src="../../images/app/11.png" width="30px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;History</a><hr>
       <a class="dropdown-item" href="setting.php"><img src="../../images/app/18.png" width="50px" >&nbsp;&nbsp;&nbsp;Setting</a>
       <a class="dropdown-item" href="logout.php"><img src="../../images/app/12.png" width="50px" >&nbsp;&nbsp;&nbsp;logout</a>
    
