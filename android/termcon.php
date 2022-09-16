5e1798e227544505ea9a22cf3c7e2cdbbc1ee824
shivam853203 <shivam.kumar@brightcodess.com>
2022-09-16 16:18:27 +0530

    update
    update

--- android/termcon.php
+++ android/termcon.php
@@ -11,6 +11,14 @@
               <p>Welcome to Educollection pratibha darpan!</p>
               <p>These terms and conditions outline the rules and regulations for the use of Swayam sahayak saamajik evam arthik vikash sansthan's Website, located at https://educollectionpratibhadarpan.com.</p>
               <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Educollection pratibha darpan if you do not agree to take all of the terms and conditions stated on this page.</p>
+              <p>Whoever joins this Organisation, they will have to think about the development of the Organisation in every way.
+                If they face any kind of difficulty in this work, then they will get all kinds of support from the Organisation.</p>
+              <p> Whoever will join the field executive in this Organisation.There is a provision not to give them any kind of field expense or salary from the Organisation.</p>
+              <p> In this organisation the person who will join the field executive will work under an independent self- employment and in return for the work the amount fixed by the organisation will be provided to them. </p>
+              <p> In case of any technical fault, the account may get delayed. for this the organisation would like to apologize And will make every effort to pay you people but if there is technical fault, no claim can be made. </p>
+              <p>  In the joining of the child in the Organisation by any worker legal action will be taken against them if the institution collects more than the prescribe fee.</p>
+              <p>The children who join above class 10th, their registration will be deactivated.
+                    Even if he Comes in the winner list he will not be given the winning amount. </p>
            </div>
         </div>
      </div>
